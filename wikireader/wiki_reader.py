6f90b878772bc1e4ebbf3233a464c3ed11af4e43
Timothee Mickus <tmickus@aquilon.home>
2018-09-29 16:05:10 +0200

    wiki reader (tmp)

--- wikireader/wiki_reader.py
+++ wikireader/wiki_reader.py
@@ -1,5 +1,4 @@
 import bs4
-import os
 import re
 
 _LANG_PATTERNS = {
@@ -27,7 +26,7 @@ _POS = [
     "Preposition",
     "Pronoun",
     "Proper noun",
-    "Verb"
+    "Verb",
 ]
 
 _POS_PATTERNS = {pos : re.compile("^===%s===$" % re.escape(pos), re.MULTILINE) for pos in _POS}
@@ -35,7 +34,6 @@ _POS_PATTERNS = {pos : re.compile("^===%s===$" % re.escape(pos), re.MULTILINE) f
 _NEXT_L2_SECTION_PATTERN = re.compile("^==[^=]+?==$", re.MULTILINE)
 
 _NEXT_L3_SECTION_PATTERN = re.compile("^===[^=]+?===$", re.MULTILINE)
-
 
 def read_pages(filename) :
     """
@@ -46,14 +44,13 @@ def read_pages(filename) :
         with open(filename, "r") as istr:
     	    pg_acc = []
     	    for line in istr :
-                line = line.strip()
-                if line == "<page>" :
+                if line.strip() == "<page>" :
                     pg_acc = []
                 pg_acc.append(line)
-                if line == "</page>" :
+                if line.strip() == "</page>" :
                     yield pg_acc
     for page in _yield_raw(filename) :
-        page_soup = bs4.BeautifulSoup((os.linesep).join(page), "lxml")
+        page_soup = bs4.BeautifulSoup("".join(page), "lxml")
         title = page_soup.find("title")
         if not title : continue
         title = title.get_text()
@@ -64,19 +61,19 @@ def read_pages(filename) :
 
 def page_contains_lang(page_text, lang_code) :
     """
-    Check whether page contains entry for Language
+    Checks whether page contains entry for Language
     """
     return _LANG_PATTERNS[lang_code].match(page_text)
 
 def page_contains_pos(page_text, pos_name):
     """
-    Check whether page contains entry for Part of Speech
+    Checks whether page contains entry for Part of Speech
     """
     return _POS_PATTERNS[pos_name].match(page_text)
 
 def extract_lang_section(page_text, lang_code) :
     """
-    Retrieve relevant wiki section for language
+    Retrieves relevant wiki section for language
     """
     lang_section = page_contains_lang(page_text, lang_code)
     if lang_section :
@@ -88,7 +85,7 @@ def extract_lang_section(page_text, lang_code) :
 
 def extract_pos_section(page_text, pos_name) :
     """
-    Retrieve relevant wiki section for PoS
+    Retrieves relevant wiki section for PoS
     """
     pos_section = page_contains_pos(page_text, pos_name)
     if pos_section :
@@ -100,7 +97,7 @@ def extract_pos_section(page_text, pos_name) :
 
 def retrieve_definitions(filepath, extraction_dictionary) :
     """
-    Retrieve definitions as specified by `extraction_dictionary`
+    Retrieves definitions as specified by `extraction_dictionary`
     """
     for title, text in read_pages(filepath) :
         for lang in extraction_dictionary :
@@ -111,14 +108,10 @@ def retrieve_definitions(filepath, extraction_dictionary) :
                         pos_section = extract_pos_section(lang_section, pos)
                         yield title, lang, pos, pos_section
                     if pos in lang_section :
-                        print((lang_section,_POS_PATTERNS[pos]))
-                        exit()
-
+                        print("err")
+                        exit(1)
 
 if __name__ == "__main__" :
     filepath = "../../Desktop/enwiktionary-20180901-pages-meta-current.xml"
     extraction_dictionary = {'EN': ['Noun']}
-    print(_LANG_PATTERNS['EN'])
-    print(_POS_PATTERNS['Noun'])
-
     print(next(retrieve_definitions(filepath, extraction_dictionary)))
